3163e9a4daae94a00759e5f5b9104e602e7beec1
Warren Strange <warren.strange@gmail.com>
2016-09-08 10:21:52 -0600

    openig logback

--- openig/run-openig.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Start OpenIG enabled Tomcat
-# To create a persistent configuration mount a data volume on /root/config
-
-mkdir -p /root/openig
-
-# Start Tomcat
-exec /usr/local/tomcat/bin/catalina.sh run
